31b700db697622dc321dc4a3b3d0c4cfbcc41069
sagar-kumar-yadav <sk5323912@gmail.com>
2023-07-04 14:13:57 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = "2UfvhbZv3dlWq7AUgv6VeFZZqW3aIbj4wDaLJrPSudE";
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResult = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
